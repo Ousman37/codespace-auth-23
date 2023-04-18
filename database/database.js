4d9a69effb7844b6dfe7d6248b0590cfb6ad9dbf
Didi Ethmane <95219508+Ousman37@users.noreply.github.com>
2023-04-18 10:24:23 +0000

    Setup connection mongodb

--- database/database.js
+++ database/database.js
@@ -2,19 +2,20 @@
 
 const mongoose = require('mongoose');
 
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost/myapp';
+const MONGODB_URL = process.env;
 
 exports.connect = () => {
-  mongoose.connect(MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true, // add this to enable index creation
-    useFindAndModify: false // add this to disable deprecated find and modify methods
-  })
+  mongoose
+    .connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true, // add this to enable index creation
+      useFindAndModify: false, // add this to disable deprecated find and modify methods
+    })
     .then(() => {
       console.log('MongoDB connected successfully');
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(`MongoDB connection error: ${error}`);
       process.exit(1);
     });
